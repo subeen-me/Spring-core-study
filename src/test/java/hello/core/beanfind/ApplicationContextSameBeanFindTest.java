d2e10114f9bdc362cf0d4dfbcd2ecd5ed4742174
subeen-me <subeen.me@gmail.com>
2022-02-08 20:02:29 +0900

    Autowired test

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -29,12 +29,12 @@ public class ApplicationContextSameBeanFindTest {
                () -> ac.getBean(MemberRepository.class));
     }
 
-    @Test
-    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면 빈 이름을 지정하면 된다")
-    void findBeanByName() {
-        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
-        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
-    }
+//    @Test
+//    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면 빈 이름을 지정하면 된다")
+//    void findBeanByName() {
+//        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+//        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+//    }
 
     @Test
     @DisplayName("특정 타입을 모두 조회하기")
